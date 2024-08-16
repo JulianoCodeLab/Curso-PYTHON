b79884b24c08ecf155c1a457bbd44fbcfc2e39b7
Juliano Ambrósio <gestaojuliano99@gmail.com>
2024-08-16 16:56:55 -0300

    aula 4 atualizada

--- aula4.py
+++ aula4.py
@@ -2,3 +2,27 @@
 Operadores de atribuições
 = += -= *= /= //= **= %=
 """
+objeto = 1
+
+###
+
+#contador = contador + 1
+objeto += 3
+print(objeto)
+
+
+#----------------------------------------------------------------#
+#funciona tambem com string
+
+contadorStr = '1'
+
+contadorStr += '0'
+print(contadorStr)
+
+
+#----------------------------------------------------------------#
+#o multiplicador repete o atributo do objeto N vezes se eu multiplicar uma string
+contadorMt = '2'
+
+contadorMt *= 10
+print(contadorMt)
